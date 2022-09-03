04693e97310ec1068e63a8e3d81ec52369df7bc3
mauv2sky <mauv2sky@gmail.com>
2022-09-03 21:23:49 +0900

    [Modify] BJ 1094 막대기 / 주석추가

--- Baekjoon/bitmask/BJ1094_막대기.java
+++ Baekjoon/bitmask/BJ1094_막대기.java
@@ -11,8 +11,8 @@ public class BJ1094_막대기 {
 		X = sc.nextInt();
 		cnt = 0;
 		
-		for (int i = 0; i <= 6; i++) {
-			if((X & (1<<i)) != 0) cnt++;
+		for (int i = 0; i <= 6; i++) { // 64의 이진수 길이만큼 반복
+			if((X & (1<<i)) != 0) cnt++; // 이진수로 변환한 X의 1의 갯수 세기
 		}
 		System.out.println(cnt);
 	}
