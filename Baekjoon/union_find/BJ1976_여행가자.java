74dc7fff3c28eb3aeb9fcacf34c557e3ac83e677
mauv2sky <mauv2sky@gmail.com>
2022-09-05 22:37:06 +0900

    [Modify] BJ 1976 여행가자 / 주석추가

--- Baekjoon/union_find/BJ1976_여행가자.java
+++ Baekjoon/union_find/BJ1976_여행가자.java
@@ -22,6 +22,7 @@ public class BJ1976_여행가자 {
 			parent[i] = i;
 		}
 		
+		// 간선이 있다면 두 도시 union 처리
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
@@ -32,13 +33,14 @@ public class BJ1976_여행가자 {
 			}
 		}
 		
+		// 여행 도시 리스트 입력
 		list = new int[M];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
 			list[i] = Integer.parseInt(st.nextToken()) - 1;
 		}
 		
-		
+		// 갈 수 있는 여행 경로인지 판단
 		res = 1;
 		for (int i = 0; i < M-1; i++) {
 			if(parent[list[i]] != parent[list[i+1]]) {
@@ -55,7 +57,7 @@ public class BJ1976_여행가자 {
 		return parent[x] = findParent(parent[x]);
 	}
 	
-	static void union(int a, int b) {
+	static void union(int a, int b) { // 부모의 부모를 갱신
 		a = findParent(a);
 		b = findParent(b);
 		
